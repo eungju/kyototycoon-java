b2501597e4c21d7e3c744cda2faf24d8186fb538
Eungju PARK <eungju at gmail dot com>
2011-07-05 07:42:13 +0900

    Performance test for increments on the same record.

--- src/test/java/kyototycoon/PerformanceTest.java
+++ src/test/java/kyototycoon/PerformanceTest.java
@@ -3,11 +3,14 @@ package kyototycoon;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class PerformanceTest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     RemoteDB dut;
 
     @Before
@@ -32,9 +35,24 @@ public class PerformanceTest {
 		    dut.get("key");
         }
         watch.stop();
-        System.out.println("Elapsed: " + watch.elapsed());
+        logger.info("Elapsed: {}", watch.elapsed());
         double tps = n / ((double) watch.elapsed() / 1000);
-        System.out.println("TPS: " + tps);
+        logger.info("TPS: {}", tps);
         assertThat(tps, greaterThan(300D));
 	}
+
+    @Test
+    public void incrementsOnSameKey() throws Exception {
+        StopWatch watch = new StopWatch();
+        final int n = 1000;
+        watch.start();
+        for (int i = 0; i < n; i++) {
+            dut.increment("hit", 1);
+        }
+        watch.stop();
+        logger.info("Elapsed: {}", watch.elapsed());
+        double tps = n / ((double) watch.elapsed() / 1000);
+        logger.info("TPS: {}", tps);
+        assertThat(tps, greaterThan(300D));
+    }
 }
