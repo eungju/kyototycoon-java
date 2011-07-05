d240e6e8f84d287927fa4d761b274f06b3f79fcd
Eungju PARK <eungju at gmail dot com>
2011-07-06 08:27:26 +0900

    Extract TsvRpcConnection.

--- src/main/java/kyototycoon/RemoteDB.java
+++ src/main/java/kyototycoon/RemoteDB.java
@@ -1,57 +1,41 @@
 package kyototycoon;
 
-import org.eclipse.jetty.client.Address;
-import org.eclipse.jetty.client.ContentExchange;
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.http.HttpVersions;
-import org.eclipse.jetty.io.ByteArrayBuffer;
-
-import java.io.IOException;
-
 public class RemoteDB {
-    private final HttpClient httpClient;
-    private final Address address;
+    private final TsvRpcConnection connection;
     private final StringTranscoder stringTranscoder = StringTranscoder.INSTANCE;
     private Transcoder keyTranscoder = StringTranscoder.INSTANCE;
     private Transcoder valueTranscoder = StringTranscoder.INSTANCE;
 
     public RemoteDB(String host, int port) throws Exception {
-        this.address = new Address(host, port);
-        httpClient = new HttpClient();
-        httpClient.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
-        httpClient.start();
+        connection = new TsvRpcConnection(host, port);
     }
 
     public void destroy() {
-        try {
-            httpClient.stop();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        connection.close();
     }
 
     public void set(String key, Object value) {
-        call("set", new Values().put("key", keyTranscoder.encode(key)).put("value", valueTranscoder.encode(value)));
+        connection.call("set", new Values().put("key", keyTranscoder.encode(key)).put("value", valueTranscoder.encode(value)));
     }
 
     public Object get(String key) {
-        Values output = call("get", new Values().put("key", keyTranscoder.encode(key)));
+        Values output = connection.call("get", new Values().put("key", keyTranscoder.encode(key)));
         byte[] value = output.get("value");
         return value == null ? null : valueTranscoder.decode(value);
     }
 
     public void clear() {
-        call("clear", new Values());
+        connection.call("clear", new Values());
     }
 
     public long increment(String key, long num) {
-        Values output = call("increment", new Values().put("key", keyTranscoder.encode(key)).put("num", stringTranscoder.encode(String.valueOf(num))));
+        Values output = connection.call("increment", new Values().put("key", keyTranscoder.encode(key)).put("num", stringTranscoder.encode(String.valueOf(num))));
         checkError(output);
         return Long.parseLong(stringTranscoder.decode(output.get("num")));
     }
 
     public double incrementDouble(String key, double num) {
-        Values output = call("increment_double", new Values().put("key", keyTranscoder.encode(key)).put("num", stringTranscoder.encode(String.valueOf(num))));
+        Values output = connection.call("increment_double", new Values().put("key", keyTranscoder.encode(key)).put("num", stringTranscoder.encode(String.valueOf(num))));
         checkError(output);
         return Double.parseDouble(stringTranscoder.decode(output.get("num")));
     }
@@ -60,38 +44,6 @@ public class RemoteDB {
         byte[] error = output.get("ERROR");
         if (error != null) {
             throw new RuntimeException(stringTranscoder.decode(error));
-        }
-    }
-
-    Values call(String command, Values input) {
-        TsvEncoding requestEncoding = TsvEncodingHelper.forEfficiency(input);
-        try {
-            ContentExchange exchange = new ContentExchange(true);
-            exchange.setAddress(address);
-            exchange.setMethod("POST");
-            exchange.setURI("/rpc/" + command);
-            exchange.setVersion(HttpVersions.HTTP_1_1);
-            exchange.setRequestContentType(requestEncoding.contentType);
-            byte[] contentSource = requestEncoding.encode(input);
-            exchange.setRequestHeader("Content-Length", String.valueOf(contentSource.length));
-            exchange.setRequestContent(new ByteArrayBuffer(contentSource));
-            httpClient.send(exchange);
-            exchange.waitForDone();
-            TsvEncoding responseEncoding = TsvEncodingHelper.forContentType(exchange.getResponseFields().getStringField("Content-Type"));
-            Values output;
-            if (exchange.getResponseFields().getLongField("Content-Length") != 0) {
-                output = responseEncoding.decode(exchange.getResponseContentBytes());
-            } else {
-                output = new Values();
-            }
-            if (exchange.getResponseStatus() != 200 && exchange.getResponseStatus() != 450) {
-                throw new RuntimeException(stringTranscoder.decode(output.get("ERROR")));
-            }
-            return output;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
