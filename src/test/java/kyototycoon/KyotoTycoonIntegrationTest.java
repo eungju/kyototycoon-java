c1c7ecac117f5fa0e30a20faea6019f8baa78837
Eungju PARK <eungju at gmail dot com>
2011-08-03 10:49:45 +0900

    Rename the tests.

--- src/test/java/kyototycoon/KyotoTycoonIntegrationTest.java
+++ src/test/java/kyototycoon/KyotoTycoonIntegrationTest.java
@@ -27,143 +27,162 @@ public class KyotoTycoonIntegrationTest {
         dut.stop();
     }
 
-    @Test public void get_the_report_of_the_server_information() {
+    @Test public void
+    report_returns_the_report_of_the_server_information() {
         Map<String, String> actual = dut.report();
         System.out.println(actual);
         assertThat(actual, Matchers.<Object>notNullValue());
     }
 
-    @Test public void get_the_miscellaneous_status_information_of_a_database() {
+    @Test public void
+    status_returns_the_miscellaneous_status_information_of_a_database() {
         Map<String, String> actual = dut.status();
         System.out.println(actual);
         assertThat(actual, Matchers.<Object>notNullValue());
     }
 
-    @Test public void remove_all_records_in_a_database() {
+    @Test public void
+    clear_removes_all_records_in_a_database() {
         dut.set("key", "value");
         dut.clear();
         assertThat(dut.get("key"), nullValue());
     }
 
-    @Test public void synchronize_updated_contents_with_the_file_and_the_device() {
+    @Test public void
+    synchronize_synchronizes_updated_contents_with_the_file_and_the_device() {
         dut.synchronize(true);
     }
 
-    @Test public void set_the_value_of_a_record() {
+    @Test public void
+    set_sets_the_value_of_a_record() {
         dut.set("key", "value");
         assertThat((String) dut.get("key"), is("value"));
+        dut.set("key", "other");
+        assertThat((String) dut.get("key"), is("other"));
     }
 
-    @Test public void add_a_record() {
+    @Test public void
+    add_sets_the_value_of_a_record_if_record_does_not_exist() {
         dut.add("key", "value");
         assertThat((String) dut.get("key"), is("value"));
     }
 
-    @Test(expected=RuntimeException.class) public void add_a_existing_record() {
-        dut.add("key", "value");
+    @Test(expected=RuntimeException.class) public void
+    add_throws_exception_if_a_record_exist() {
+        dut.set("key", "value");
         dut.add("key", "value");
     }
 
-    @Test public void replace_the_value_of_a_record() {
-        dut.add("key", "1");
+    @Test public void
+    replace_sets_the_value_of_a_record_if_a_record_exist() {
+        dut.set("key", "1");
         dut.replace("key", "2");
         assertThat((String) dut.get("key"), is("2"));
     }
 
-    @Test(expected=RuntimeException.class)
-    public void replace_the_value_of_a_non_existing_record() {
+    @Test(expected=RuntimeException.class) public void
+    replace_throws_exception_if_record_does_not_exist() {
         dut.replace("key", "value");
     }
 
-    @Test public void append_the_value_of_a_record() {
+    @Test public void
+    append_appends_the_value_to_the_value_of_a_record() {
         dut.add("key", "1");
         dut.append("key", "23");
         assertThat((String) dut.get("key"), is("123"));
     }
 
-    @Test public void append_the_value_of_a_non_existing_record() {
+    @Test public void
+    append_sets_the_value_of_a_record_if_record_does_not_exist() {
         dut.append("key", "23");
         assertThat((String) dut.get("key"), is("23"));
     }
     
     @Test public void
-    add_a_numeric_integer_to_the_numeric_integer_value_of_a_record() {
-    	dut.increment("key", 3L, IncrementOrigin.ZERO, ExpirationTime.NONE);
+    increment_adds_the_number_to_the_value_of_a_record() {
+    	dut.increment("key", 3L);
     	assertThat(dut.increment("key", 4), is(7L));
     }
 
     @Test public void
-    add_a_numeric_integer_value_to_a_non_existing_record() {
+    increment_sets_the_number_if_record_does_not_exist() {
     	assertThat(dut.increment("key", 3), is(3L));
     }
 
     @Test(expected=RuntimeException.class) public void
-    add_a_numeric_integer_value_to_the_non_numeric_integer_value_of_a_record() {
+    increment_throws_exception_if_the_value_of_a_record_is_not_a_numeric_integer() {
         dut.set("key", "3");
         dut.increment("key", 4L);
     }
 
     @Test public void
-    set_the_numeric_integer_value_of_a_record() {
+    increment_sets_the_value_of_a_record_if_the_origin_is_set() {
         dut.increment("key", 3L);
         assertThat(dut.increment("key", 4L, IncrementOrigin.SET, ExpirationTime.NONE), is(4L));
     }
 
     @Test(expected=RuntimeException.class) public void
-    try_to_set_the_numeric_integer_value_of_a_record() {
+    increment_throws_exception_if_the_origin_is_try_and_the_value_of_a_record_is_not_a_numeric_integer() {
         dut.increment("key", 4L, IncrementOrigin.TRY, ExpirationTime.NONE);
     }
 
     @Test public void
-    add_a_numeric_double_value_to_the_numeric_double_value_of_a_record() {
-    	dut.incrementDouble("key", 0.3, IncrementOrigin.ZERO, ExpirationTime.NONE);
+    increment_double_adds_the_number_to_the_value_of_a_record() {
+    	dut.incrementDouble("key", 0.3);
     	assertThat(dut.incrementDouble("key", 0.4), is(0.7));
     }
 
     @Test public void
-    add_a_numeric_double_value_to_a_non_existing_record() {
+    increment_double_sets_the_number_if_record_does_not_exist() {
     	assertThat(dut.incrementDouble("key", 0.3), is(0.3));
     }
 
     @Test(expected=RuntimeException.class) public void
-    add_a_numeric_double_value_to_the_non_numeric_double_value_of_a_record() {
+    increment_double_throws_exception_if_the_value_of_a_record_is_not_a_numeric_double() {
         dut.set("key", "0.3");
         dut.incrementDouble("key", 0.4);
     }
 
     @Test public void
-    set_the_numeric_double_value_of_a_record() {
+    increment_double_sets_the_value_of_a_record_if_the_origin_is_set() {
         dut.incrementDouble("key", 0.3);
         assertThat(dut.incrementDouble("key", 0.4, IncrementOrigin.SET, ExpirationTime.NONE), is(0.4));
     }
 
     @Test(expected=RuntimeException.class) public void
-    try_to_set_the_double_value_of_a_record() {
+    increment_double_throws_exception_if_the_origin_is_try_and_the_value_of_a_record_is_not_a_numeric_double() {
         dut.incrementDouble("key", 0.4, IncrementOrigin.TRY, ExpirationTime.NONE);
     }
 
     @Test public void
-    compareAndSwap() {
+    cas_sets_the_value_of_a_record_if_the_old_value_matches() {
         dut.set("key", "1");
         assertThat(dut.cas("key", "1", "2"), is(true));
-        assertThat((String) dut.get("key"), is("2"));
-        assertThat(dut.cas("key", "1", "3"), is(false));
         assertThat((String) dut.get("key"), is("2"));
     }
 
     @Test public void
-    compareAndSwapRemovesTheRecord() {
+    cas_does_not_set_the_value_of_a_record_if_the_old_value_does_not_match() {
         dut.set("key", "1");
-        dut.cas("key", "1", null);
+        assertThat(dut.cas("key", "2", "3"), is(false));
+        assertThat((String) dut.get("key"), is("1"));
+    }
+
+    @Test public void
+    cas_removes_a_record_if_the_new_value_is_null() {
+        dut.set("key", "1");
+        assertThat(dut.cas("key", "1", null), is(true));
         assertThat(dut.get("key"), nullValue());
     }
 
-    @Test public void remove_a_record() {
+    @Test public void
+    remove_removes_a_record() {
         dut.set("key", "value");
         assertThat(dut.remove("key"), is(true));
     }
 
-    @Test public void remove_a_non_existing_record() {
+    @Test public void
+    remove_returns_false_if_record_does_not_exist() {
         assertThat(dut.remove("key"), is(false));
     }
 
@@ -174,7 +193,7 @@ public class KyotoTycoonIntegrationTest {
     }
 
     @Test public void
-    get_returns_null_when_a_record_is_not_exist() {
+    get_returns_null_if_record_does_not_exist() {
         assertThat(dut.get("key"), nullValue());
     }
 
@@ -186,7 +205,7 @@ public class KyotoTycoonIntegrationTest {
     }
 
     @Test public void
-    seize_returns_null_when_a_record_is_not_exist() {
+    seize_returns_null_if_record_does_not_exist() {
         assertThat((String) dut.seize("key"), nullValue());
         assertThat(dut.get("key"), nullValue());
     }
