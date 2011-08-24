bf4b9bb4b4f7b6d08f674a53f27cb533eed3adaa
Eungju PARK <eungju at gmail dot com>
2011-08-24 10:50:15 +0900

    Allow more connections.

--- src/main/java/kyototycoon/finagle/FinagleTsvRpcClient.java
+++ src/main/java/kyototycoon/finagle/FinagleTsvRpcClient.java
@@ -37,7 +37,7 @@ public class FinagleTsvRpcClient extends FinagleTsvRpc implements TsvRpcClient {
     ServiceFactory<TsvRpcRequest, TsvRpcResponse> buildServiceFactory() {
         ClientBuilder builder = ClientBuilder.get()
                 .codec(new FinagleTsvRpcCodec())
-                .hostConnectionLimit(1);
+                .hostConnectionLimit(10);
         if (requestTimeout != null) {
             builder = builder.requestTimeout(requestTimeout);
         }
