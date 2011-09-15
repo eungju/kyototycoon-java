922b80feb4e0cda9151b7e81d352a1230925b9b3
Eungju PARK <eungju at gmail dot com>
2011-09-15 17:27:53 +0900

    Drop Apache HTTP Compoents.

--- src/test/java/kyototycoon/benchmark/AhcTsvRpcBenchmark.java
+++
@@ -1,9 +0,0 @@
-package kyototycoon.benchmark;
-
-import kyototycoon.ahc.AhcTsvRpcClient;
-
-public class AhcTsvRpcBenchmark {
-	public static void main(String[] args) throws Exception {
-        new TsvRpcBenchmark(new AhcTsvRpcClient()).run();
-	}
-}
